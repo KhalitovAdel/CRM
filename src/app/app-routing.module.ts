bd8f7a8a0c4ffd6b9528a30a5a5de8fad6e3a3ea
KhalitovAdel <myhealthisgood@gmail.com>
2019-06-16 02:26:44 +0300

    added authguard to routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './CRM/WORKSPACE/login/login.component';
 import { MainMenuComponent } from './CRM/WORKSPACE/blocks/main-menu/main-menu.component';
+import { AuthGuard } from './SERVICES/authguard/auth.guard';
 
 const routes: Routes = [
-  { path: 'workspace', component: MainMenuComponent},
+  { path: 'workspace', canActivate: [AuthGuard], component: MainMenuComponent, children: [
+    //{ path: '', pathMatch: 'full', component: dwdwdw},
+  ]},
   { path: 'workspace/login', component: LoginComponent},
 ];
 
